3d33c1937923b973cc9dd227dd20d0b936cdb8c0
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-07-11 00:17:14 +0530

    Week 2: Implemented execution tracer using sys.settrace

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 conn = sqlite3.connect("pychronicle.db")
-
 cursor = conn.cursor()
 
+# Week 1 table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS variables(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,8 +11,19 @@ CREATE TABLE IF NOT EXISTS variables(
 )
 """)
 
+# Week 2 table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS execution(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    line_number INTEGER,
+    variable_name TEXT,
+    variable_value TEXT
+)
+""")
+
 conn.commit()
 
-conn.close()
-
 print("Database Created Successfully")
+print("Execution Table Created Successfully")
+
+conn.close()
--- tracer.py
+++ tracer.py
@@ -1,8 +1,46 @@
 import sys
 
 def trace_function(frame, event, arg):
-    print("TRACE:", event, "Line:", frame.f_lineno)
+    if event == "line":
+        print(f"Event: {event}")
+        print(f"Line Number: {frame.f_lineno}")
+        print("---------------------")
     return trace_function
+
+sys.settrace(trace_function)
+
+x = 10
+y = 20
+z = x + y
+
+print(z)
+
+sys.settrace(None)   # Stop tracing
+import sys
+import sqlite3
+
+# Connect to database
+conn = sqlite3.connect("pychronicle.db")
+cursor = conn.cursor()
+
+def trace_function(frame, event, arg):
+    if event == "line":
+
+        line = frame.f_lineno
+
+        for var, value in frame.f_locals.items():
+
+            cursor.execute("""
+                INSERT INTO execution(line_number, variable_name, variable_value)
+                VALUES (?, ?, ?)
+            """, (line, var, str(value)))
+
+            conn.commit()
+
+            print(f"Line {line} | {var} = {value}")
+
+    return trace_function
+
 
 def main():
     x = 10
@@ -10,6 +48,11 @@ def main():
     z = x + y
     print(z)
 
+
 sys.settrace(trace_function)
+
 main()
+
 sys.settrace(None)
+
+conn.close()
